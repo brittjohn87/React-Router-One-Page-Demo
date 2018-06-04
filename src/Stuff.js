2b52308990943fb05e9518fdc745ed998ce36368
brittjohn87 <brittanytjohnson@icloud.com>
2018-06-04 15:24:31 -0400

    first commit

---
+++ src/Stuff.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Stuff extends Component {
+  render(){
+    return(
+      <div>
+        <h2>STUFF</h2>
+        <p>Mauris sem velit, vehicula eget sodales vitae,
+        rhoncus eget sapien:</p>
+        <ol>
+          <li>Nulla Diam</li>
+          <li>Facilisis Bibendum</li>
+          <li>Eget Erat</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
